64a226d33b90c715923f5cb8dd111583cf7e5ef0
v.zakharzhevskyi <vladyslav.zr@gmail.com>
2023-11-21 16:43:07 +0200

    move out unused imports;

--- src/main/java/com/starv/micro/goods/conf/CassandraConfig.java
+++ src/main/java/com/starv/micro/goods/conf/CassandraConfig.java
@@ -1,18 +1,12 @@
 package com.starv.micro.goods.conf;
 
 import com.datastax.oss.driver.api.core.ConsistencyLevel;
-import com.datastax.oss.driver.api.core.CqlIdentifier;
 import com.starv.micro.goods.model.AppCassandraConnectionDetails;
 import org.springframework.boot.autoconfigure.cassandra.CassandraConnectionDetails;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.cassandra.ReactiveSession;
 import org.springframework.data.cassandra.ReactiveSessionFactory;
-import org.springframework.data.cassandra.core.ReactiveCassandraOperations;
-import org.springframework.data.cassandra.core.ReactiveCassandraTemplate;
-import org.springframework.data.cassandra.core.convert.CassandraConverter;
 import org.springframework.data.cassandra.core.cql.ReactiveCqlTemplate;
 
 import java.util.List;
